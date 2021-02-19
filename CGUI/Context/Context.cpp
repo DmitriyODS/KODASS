89454ff88c464e4133c87b0d400a998fa072d750
Dmitriy <osipovskij.dima@yandex.ru>
2021-02-19 14:50:15 +0300

    ???

--- CGUI/Context/Context.cpp
+++ CGUI/Context/Context.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Context.h"
+
+
+namespace kds {
+
+    Theme &Context::getTheme() const {
+        return m_theme;
+    }
+
+    Context::Context(CManager &c_manager, Theme &theme)
+            : m_c_manager(c_manager), m_theme(theme) {
+    }
+
+    CManager &Context::getCManager() const {
+        return m_c_manager;
+    }
+}
--- CGUI/Context/Context.h
+++ CGUI/Context/Context.h
@@ -2,13 +2,34 @@
 // Created by osipo on 18.02.2021.
 //
 
+/*
+ * Класс контекста всего приложения, который должен быть унаследован
+ */
+
 #ifndef KODASS_CONTEXT_H
 #define KODASS_CONTEXT_H
 
+#include <Theme.h>
+#include "../CManager/ConsoleAPI.h"
 
-class Context {
 
-};
+namespace kds {
+
+    class Context {
+    public:
+
+        explicit Context(CManager &c_manager, Theme &theme);
+
+        [[nodiscard]] Theme &getTheme() const;
+
+        [[nodiscard]] CManager &getCManager() const;
+
+    protected:
+        Theme &m_theme;
+        CManager &m_c_manager;
+    };
+
+}
 
 
 #endif //KODASS_CONTEXT_H
